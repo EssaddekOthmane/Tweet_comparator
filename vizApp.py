4969417acd1970e3474904413200a1a7b2c18148
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-01-31 15:13:59 +0100

    Update vizApp.py

--- vizApp.py
+++ vizApp.py
@@ -4,15 +4,41 @@ from datetime import datetime
 import altair as alt
 import streamlit as st
 
+def lasq_date(x):
+    
+    y=x[:10]
+    d=x[11:len(x)-5]
+    dd=y+' '+d
+    return(datetime.strptime(dd, '%Y-%m-%d %H:%M:%S'))
+        
+
+def lasq_heure(x):
+    
+    
+    d=x[11:len(x)-5]
+
+    return(datetime.strptime(d, '%H:%M:%S'))
+        
+
 
 macron=pd.read_csv('DF_mac.csv')
 zemour=pd.read_csv('DF_zem.csv')
 mel=pd.read_csv('Df_mell.csv')
 
+macron['daate'] = macron['Timestamp'].apply( lambda x : lasq_date(x) )
+macron['heuure'] = macron['Timestamp'].apply( lambda x : lasq_heure(x) )
 macron['month'] = macron['date'].apply(lambda d: d.month)
 
+zemour['daate'] = zemour['Timestamp'].apply( lambda x : lasq_date(x) )
+zemour['heuure'] = zemour['Timestamp'].apply( lambda x : lasq_heure(x) )
 zemour['month'] = zemour['date'].apply(lambda d: d.month)
+
+mel['daate'] = mel['Timestamp'].apply( lambda x : lasq_date(x) )
+mel['heuure'] = mel['Timestamp'].apply( lambda x : lasq_heure(x) )
 mel['month'] = mel['date'].apply(lambda d: d.month)
+
+# zemour['month'] = zemour['date'].apply(lambda d: d.month)
+# mel['month'] = mel['date'].apply(lambda d: d.month)
 
 mDf=pd.read_csv('occurM.csv')
 diccc=mDf.set_index('Unnamed: 0').T.to_dict('list')
@@ -61,16 +87,16 @@ class Candidat(str):
 
         if str is 'Zmmour':
 
-            self.data = zemour[dd1<zemour['date']]
-            self.data=self.data[self.data['date']<dd2]
+            self.data = zemour[dd1<zemour['daate']]
+            self.data=self.data[self.data['daate']<dd2]
 
         if str is 'Macron':
-            self.data=macron[dd1<macron['date']]
-            self.data=self.data[self.data['date']<dd2]
+            self.data=macron[dd1<macron['daate']]
+            self.data=self.data[self.data['daate']<dd2]
         
         if str is 'Mellonchon':
-            self.data=mel[dd1<mel['date']]
-            self.data=self.data[self.data['date']<dd2]
+            self.data=mel[dd1<mel['daate']]
+            self.data=self.data[self.data['daate']<dd2]
     
 
 
@@ -82,26 +108,26 @@ base2=alt.Chart(Abstractpol2.data)
 
 if var=='re-tweets':
     line11 = base1.mark_line().encode(
-        x='month',
+        x='daate',
         y=E[0],)
     line12 = base2.mark_line(color='red').encode(
-        x='month',
+        x='daate',
         y=E[0],)
 
 if var=='commentaires':
     line11 = base1.mark_line().encode(
-       x='month',
+       x='daate',
         y=E[1],)
     line12 = base2.mark_line(color='red').encode(
-        x='month',
+        x='daate',
         y=E[1],)
 
 if var=='likes':
     line11 = base1.mark_line().encode(
-        x='month',
+        x='daate',
         y=E[2],)
     line12 = base2.mark_line(color='red').encode(
-        x='month',
+        x='daate',
         y=E[2],)
     
 
